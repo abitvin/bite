48f6bdf6953c08e9776127c8436f38e835d792cf
Vincent van Ingen <info@abitvin.net>
2023-06-13 23:39:31 +0200

    Added sub().

--- libsyntax/src/lib.rs
+++ libsyntax/src/lib.rs
@@ -1,3 +1,7 @@
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
+
+pub fn sub(left: usize, right: usize) -> usize {
+    left - right
+}
---
+++ libsyntax/tests/sub.rs
@@ -0,0 +1,6 @@
+use libsyntax::sub;
+
+#[test]
+fn five_minus_three() {
+    assert_eq!(sub(5, 3), 2);
+}
