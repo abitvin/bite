9c792bda6ddd5809f7e514cd189cf99301357435
Vincent van Ingen <code@abitvin.com>
2023-06-16 16:00:46 +0200

    Made a draft of the parser using combined Grammar structs.

--- libsyntax/src/lib.rs
+++ libsyntax/src/lib.rs
@@ -1,5 +1,23 @@
 extern crate grammar;
-use grammar::{CompiledGrammar, Grammar};
+use grammar::{CompiledGrammar, Grammar, GrammarError};
+
+pub struct Parser{
+    parser_grouped_7: CompiledGrammar<BoxedSeven>
+}
+
+impl Parser {
+    pub fn new() -> Self {
+        let parser_7 = parser_7();
+        let parser_grouped_7 = parser_grouped_7(parser_7);
+        
+        Self { parser_grouped_7 }
+    }
+
+    pub fn scan(&self, expr: &str) -> Result<BoxedSeven, GrammarError> {
+        self.parser_grouped_7.scan("grouped", expr)
+            .map(|mut x| x.swap_remove(0))
+    }
+}
 
 pub struct BoxedSeven(i32);
 
@@ -9,10 +27,10 @@ fn parser_7() -> CompiledGrammar<i32> {
     g.compile().unwrap()
 }
 
-pub fn parser_grouped_7() -> CompiledGrammar<BoxedSeven> {
+fn parser_grouped_7(parser_7: CompiledGrammar<i32>) -> CompiledGrammar<BoxedSeven> {
     let mut g = Grammar::new();
-    g.map("seven", "7", |_, s| {
-        parser_7()
+    g.map("seven", "7", move |_, s| {
+        parser_7
             .scan("seven", s)
             .map(|x| BoxedSeven(x[0]))
             .map_err(|x| x.to_string())
--- libsyntax/tests/parse_test.rs
+++ libsyntax/tests/parse_test.rs
@@ -1,15 +1,15 @@
-use libsyntax::parser_grouped_7;
+use libsyntax::Parser;
 
 #[test]
 fn parse_the_grouped_7() {
-    let parser = parser_grouped_7();
+    let parser = Parser::new();
     
-    let result = parser.scan("grouped", "7");
+    let result = parser.scan("7");
     assert!(result.is_err());
 
-    let result = parser.scan("grouped", "(7)");
+    let result = parser.scan("(7)");
     assert!(result.is_ok());
 
-    let result = parser.scan("grouped", "(8)");
+    let result = parser.scan("(8)");
     assert!(result.is_err());
 }
