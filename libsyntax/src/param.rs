ec3f7fd2a674c8e1262e9c26a0744a14ef06abc0
Vincent van Ingen <code@abitvin.com>
2023-06-22 00:19:18 +0200

    Implemented parsing a parameter.

--- libsyntax/src/param.rs
+++ libsyntax/src/param.rs
@@ -1,13 +1,30 @@
+use crate::{scanner::Scanner, common::parse_id};
+
+#[derive(Debug, PartialEq)]
 pub struct Param {
     pub id: String,
     pub typ: String,
 }
 
 impl Param {
-    fn new(id: &str, typ: &str) -> Self {
+    pub fn new(id: &str, typ: &str) -> Self {
         Self { 
             id: String::from(id), 
             typ: String::from(typ),
         }
+    }
+
+    pub fn parse(mut scn: &mut Scanner) -> Option<Self> {
+        scn.scan("(")?;
+        scn.scan_spaces();
+        let id = parse_id(&mut scn)?;
+        scn.scan_spaces();
+        scn.scan(":")?;
+        scn.scan_spaces();
+        let typ = parse_id(&mut scn)?;
+        scn.scan_spaces();
+        scn.scan(")")?;
+
+        Some(Self { id, typ })
     }
 }
---
+++ libsyntax/tests/param.rs
@@ -0,0 +1,12 @@
+extern crate libsyntax;
+use libsyntax::{
+    param::Param,
+    scanner::Scanner,
+};
+
+#[test]
+fn parse_param() {
+    let s = "( a : B )";
+    let mut scn = Scanner::new(s);
+    assert_eq!(Param::parse(&mut scn), Some(Param::new("a", "B")));
+}
