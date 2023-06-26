4ecb47aedb99209ba4485c14269e0023abbe307b
Vincent van Ingen <code@abitvin.com>
2023-06-26 23:18:14 +0200

    Expanded Stmt and added some shorthand constructors.

--- libast/src/stmt.rs
+++ libast/src/stmt.rs
@@ -1,6 +1,17 @@
-use crate::var_decl::VarDecl;
+use crate::{block::Block, r#if::If, var_decl::VarDecl};
 
 #[derive(Debug, PartialEq)]
 pub enum Stmt {
+    If(If),
     VarDecl(VarDecl),
+}
+
+impl Stmt {
+    pub fn new_if(if_block: (bool, impl Into<Block>), elif_blocks: Vec<(bool, Block)>, else_block: impl Into<Option<Block>>) -> Self {
+        Self::If(If::new(if_block, elif_blocks, else_block))
+    }
+
+    pub fn new_var_decl(id: impl Into<String>, typ: Option<String>, expr: impl Into<String>) -> Self {
+        Self::VarDecl(VarDecl::new(id, typ, expr))
+    }
 }
