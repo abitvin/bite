324e9a8ab34228fda7abd7a9fd571b3d39c7dcd7
Vincent van Ingen <code@abitvin.com>
2023-06-26 23:17:24 +0200

    Added Into trait for else_block.

--- libast/src/if.rs
+++ libast/src/if.rs
@@ -8,11 +8,11 @@ pub struct If {
 }
 
 impl If {
-    pub fn new(if_block: (bool, impl Into<Block>), elif_blocks: Vec<(bool, Block)>, else_block: Option<Block>) -> Self {
+    pub fn new(if_block: (bool, impl Into<Block>), elif_blocks: Vec<(bool, Block)>, else_block: impl Into<Option<Block>>) -> Self {
         Self { 
             if_block: (if_block.0, if_block.1.into()),
             elif_blocks,
-            else_block,
+            else_block: else_block.into(),
         }
     }
 }
