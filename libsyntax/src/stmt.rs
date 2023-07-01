ecab2ca08e90f29c2debfde9dc3c37bead2aeb18
Vincent van Ingen <code@abitvin.com>
2023-07-01 15:40:51 +0200

    Refactoring.

--- libsyntax/src/stmt.rs
+++ libsyntax/src/stmt.rs
@@ -1,28 +1,11 @@
-use crate::scanner::{Parse, Scanner};
+use crate::{common::try_parse, scanner::{Parse, Scanner}};
 use libast::{r#if::If, stmt::Stmt, var_decl::VarDecl};
-
 
 impl Parse for Stmt {
     type Item = Self;
 
-    fn parse(scn: &mut Scanner) -> Option<Stmt> {
-        if let Some(stmt) = parse_stmt(scn, |s| If::parse(s).map(Stmt::If)) {
-            Some(stmt)
-        }
-        else {
-            parse_stmt(scn, |s| VarDecl::parse(s).map(Stmt::VarDecl))
-        }
-    }
-}
-
-fn parse_stmt(scn: &mut Scanner, f: impl Fn(&mut Scanner) -> Option<Stmt>) -> Option<Stmt> {
-    let mut try_scn = scn.clone();
-
-    if let Some(stmt) = f(&mut try_scn) {
-        scn.replace(try_scn);
-        Some(stmt)
-    }
-    else {
-        None
+    fn parse(scn: &mut Scanner) -> Option<Self> {
+        try_parse(scn, |s| If::parse(s).map(Self::If))
+        .or(try_parse(scn, |s| VarDecl::parse(s).map(Self::VarDecl)))
     }
 }
